e73246793014acb6cf71cd0fea97bca99905ec9b
Sandra Ortega <sandrita.saan@gmail.com>
2018-09-17 22:57:12 +0200

    Create setX method

--- src/main/java/es/upm/miw/iwvg/forge/junit/Point.java
+++ src/main/java/es/upm/miw/iwvg/forge/junit/Point.java
@@ -40,6 +40,10 @@ public class Point {
         return this.y;
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
     @Override
     public String toString() {
         return "Point{" +
